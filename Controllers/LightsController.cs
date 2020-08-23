21beadd7e8462097e8a81839cc0527e98dcbadef
Daniel <daniel@unimicro.no>
2020-08-23 22:41:33 +0200

    First commit

---
+++ Controllers/LightsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Unosquare.RaspberryIO;
+using Unosquare.RaspberryIO.Abstractions;
+
+namespace Herb.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LightsController : ControllerBase
+    {
+
+        private readonly ILogger<LightsController> _logger;
+
+        public LightsController(ILogger<LightsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("on")]
+        public IActionResult LightsOn()
+        {
+            var pin = Pi.Gpio[BcmPin.Gpio02];
+            pin.PinMode = GpioPinDriveMode.Output;
+            pin.Write(true);
+            return Content("Lights on");
+        }
+
+        [AllowAnonymous]
+        [HttpPost("off")]
+        public IActionResult LightsOff()
+        {
+            var pin = Pi.Gpio[BcmPin.Gpio02];
+            pin.PinMode = GpioPinDriveMode.Output;
+            pin.Write(false);
+            return Content("Lights off");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,51 @@
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Unosquare.RaspberryIO;
+using Unosquare.WiringPi;
+
+namespace Herb
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime applicationLifetime)
+        {
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            Pi.Init<BootstrapWiringPi>();
+        }
+    }
+}
